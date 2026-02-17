4f2ce92c551bb45864d4ea193b46c8a3a99365e2
hazarikakhyatispriha-png <hazarikakhyatispriha@gmail.com>
2026-02-17 10:44:09 +0530

    Update mylist.py

--- mylist.py
+++ mylist.py
@@ -1,1 +1,19 @@
 #Create any list
+from machine import Pin
+import time
+
+led1 = Pin(14,Pin.OUT)
+led2 = Pin(15,Pin.OUT)
+led3 = Pin(12,Pin.OUT)
+led4 = Pin(26,Pin.OUT)
+
+my_number = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]
+             
+#first 1 = [1,0,0,0], i[0]
+
+for i in my_number =
+    led1.value(i[0])
+    led2.value(i[1])
+    led3.value(i[2])
+    led4.value(i[3])
+    sleep.time(2)
